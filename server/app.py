dee9aa3df4957cd9343aae8797d3f4ba20c673a0
tyler kim <yoonsoo.kim11@gmail.com>
2024-01-27 16:01:24 -0500

    completed lab

--- server/app.py
+++ server/app.py
@@ -21,6 +21,42 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def by_id(id):
+    quake = Earthquake.query.filter_by(id=id).first()
+
+    if quake:
+        body = quake.to_dict()
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+    return make_response(body, status)
+
+# The view should query the database to get the earthquake with that id, 
+# and return a response containing the model attributes and values (id, location, 
+# magnitude, year) formatted as an JSON string. The response should include an error message if 
+# no row is found. Don't forget to import Earthquake from the models module.
+
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def by_mag(magnitude):
+    quakes = []
+    for quake in Earthquake.query.filter(Earthquake.magnitude >= magnitude).all():
+        quakes.append(quake.to_dict())
+
+    body = {
+        'count': len(quakes),
+        'quakes': quakes
+    }
+
+    return make_response(body, 200)
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
